338a23886611308a4d3909a049f4267aca629cd6
Ozzy <osman_saeed@hotmail.co.uk>
2022-05-10 19:36:28 +0000

    created method to puts information

--- directory.rb
+++ directory.rb
@@ -12,11 +12,23 @@ students = [
  "Joffrey Baratheon",
  "Norman Bates"
  ]
-#Print the list of students"
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-    puts student
+ 
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-#print total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)  
+  students.each do |student|
+      puts student
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{students.count} great students"
+end
+
+print_header
+print(students)
+print_footer
+
